44028afe190237ff5198779c352a5ab6d2ffa25d
gamemakerr <96029891+gamemakerr@users.noreply.github.com>
2023-02-09 02:57:38 +0900

    task2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,107 @@
 import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
+    // <div className="bg-pink-200 min-h-screen py-20 px-10 grid gap-10">
+    //   <div className="flex justify-center items-center gap-36">
+    //     <div className="bg-white p-6 w-96">
+    //       <div className="my-5 ml-1 font-3xl">Weather</div>
+    //       <div className="border-black border-2  rounded-xl mx-5 my-5 flex justify-between items-center p-5">
+    //         <div className="flex flex-col">
+    //           <span>Casius</span>
+    //           <span>mars,12am</span>
+    //         </div>
+    //         <div className="h-24 w-24 bg-red-400 rounded-full" />
+    //         <div>85˙</div>
+    //       </div>
+    //       <div className="border-black border-2  rounded-xl mx-5 my-5 flex justify-between items-center p-5">
+    //         <div className="flex flex-col">
+    //           <span>Casius</span>
+    //           <span>mars,12am</span>
+    //         </div>
+    //         <div className="h-24 w-24 bg-red-400 rounded-full" />
+    //         <div>85˙</div>
+    //       </div>
+    //       <div className="border-black border-2  rounded-xl mx-5 my-5 flex justify-between items-center p-5">
+    //         <div className="flex flex-col">
+    //           <span>Casius</span>
+    //           <span>mars,12am</span>
+    //         </div>
+    //         <div className="h-24 w-24 bg-red-400 rounded-full" />
+    //         <div>85˙</div>
+    //       </div>
+    //       <div className="border-black border-2  rounded-xl mx-5 my-5 flex justify-between items-center p-5">
+    //         <div className="flex flex-col">
+    //           <span>Casius</span>
+    //           <span>mars,12am</span>
+    //         </div>
+    //         <div className="h-24 w-24 bg-red-400 rounded-full" />
+    //         <div>85˙</div>
+    //       </div>
+    //       <button className="rounded-full bg-white border-2 border-black h-24 w-24 ml-20">
+    //         +
+    //       </button>
+    //     </div>
+    //     <div className="bg-white w-96 p-6">
+    //       <button className="border-black border-2 rounded-full h-12 w-12">
+    //         X
+    //       </button>
+    //       <div className="text-xs">simple tag</div>
+    //       <div className="text-2xl">work with best designer</div>
+    //       <div className="grid grid-cols-2 gap-5">
+    //         <div className="border-black border-2  rounded-xl h-24 w-24">1</div>
+    //         <div className="border-black border-2 rounded-xl h-24 w-24">1</div>
+    //         <div className="border-black border-2 rounded-xl h-24 w-24">1</div>
+    //         <div className="border-black border-2 rounded-xl h-24 w-24">1</div>
+    //       </div>
+    //       <button className="rounded-xl bg-purple-400">let get it done</button>
+    //     </div>
+    //     <div className="bg-yellow-500 flex flex-col p-3 space-y-5">
+    //       <div className="flex my-5">
+    //         <button className="border-black border-2 rounded-full">←</button>
+    //         <span className="font-semibold font-3xl">Friends</span>
+    //       </div>
+    //       <input
+    //         placeholder="Search with love..."
+    //         className="border-black border-2 rounded-xl"
+    //       ></input>
+    //       <div className="flex bg-white rounded-xl p-3">
+    //         <div className="w-24 h-24 bg-gray-500 rounded-full" />
+    //         <div className="flex flex-col">
+    //           <span className="text-2xl">sangha park</span>
+    //           <span className="text-xs">sangha park</span>
+    //         </div>
+    //       </div>
+    //       <div className="flex bg-white rounded-xl p-3">
+    //         <div className="w-24 h-24 bg-gray-500 rounded-full" />
+    //         <div className="flex flex-col">
+    //           <span className="text-2xl">sangha park</span>
+    //           <span className="text-xs">sangha park</span>
+    //         </div>
+    //       </div>
+    //       <div className="flex bg-white rounded-xl p-3">
+    //         <div className="w-24 h-24 bg-gray-500 rounded-full" />
+    //         <div className="flex flex-col">
+    //           <span className="text-2xl">sangha park</span>
+    //           <span className="text-xs">sangha park</span>
+    //         </div>
+    //       </div>
+    //       <div className="flex bg-white rounded-xl p-3">
+    //         <div className="w-24 h-24 bg-gray-500 rounded-full" />
+    //         <div className="flex flex-col">
+    //           <span className="text-2xl">sangha park</span>
+    //           <span className="text-xs">sangha park</span>
+    //         </div>
+    //       </div>
+    //       <div className="flex bg-white rounded-xl p-3">
+    //         <div className="w-24 h-24 bg-gray-500 rounded-full" />
+    //         <div className="flex flex-col">
+    //           <span className="text-2xl">sangha park</span>
+    //           <span className="text-xs">sangha park</span>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
     <div className="bg-slate-400 py-20 px-10 grid gap-10 min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-xl">
         <span className="font-semibold text-2xl">Select Item</span>
