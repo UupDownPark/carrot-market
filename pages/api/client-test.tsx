e7db6af263388fe26c2616cc41ee8a8aefb94c7d
gamemakerr <96029891+gamemakerr@users.noreply.github.com>
2023-02-20 23:41:25 +0900

    dd

--- pages/api/client-test.tsx
+++ pages/api/client-test.tsx
@@ -5,7 +5,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  await client.user.create({ data: { email: "hi", name: "hi" } });
+  await client.user.create({ data: { email: "hello", name: "hi" } });
 
   res.json({
     ok: true,
